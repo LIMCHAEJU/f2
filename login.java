30b6665d372a68651c10be71dd2c764daa95dc5c
LIMCHAEJU <127743640+LIMCHAEJU@users.noreply.github.com>
2023-06-16 23:43:55 +0900

    Add files via upload

---
+++ login.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class login {
+  String IDarray[];
+  IDarray = new String[100];
+  String passWordArray[];
+  passWordArray = new String [100];
+  
+  System.out.print("ID를 입력하세요 :");
+  Scanner scanner = new Scanner(System.in);
+  String ID = scanner.next();
+  System.out.print("비밀번호를 입력하세요 :");
+  String passWord = scanner.next();
+
+}
